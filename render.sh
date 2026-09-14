10bb5c40446960cdbf4ebad5a76f9bb3d08a0214
Bear0325 <lsleishi@bnbu.edu.cn>
2026-09-14 14:48:50 +0800

    Delete render.sh

--- render.sh
+++
@@ -1,8 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-subjects=(074)
-for SUBJECT in "${subjects[@]}"; do
-    python render.py \
-    --iteration 240000\
-    -m output/UNION10EMOEXP_${SUBJECT}_eval_600k\
-    --skip_train
-done
